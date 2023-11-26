5857d6aad54974640c613bb061be9db3a4dbd4b6
Tim <timothy.sereda@outlook.com>
2023-11-25 20:06:52 -0600

    something works

--- README.md
+++ README.md
@@ -42,7 +42,12 @@ Install Tailwind CSS:
 
 ```npm install -D tailwindcss```
 ```npx tailwindcss init```
+```pip install librosa```
+```pip install nvidia-cublas-cu11 nvidia-cudnn-cu11```
 
+```export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'```
+```pip install faster-whisper```
+```pip install opus-fast-mosestokenizer```
 # Running the Application
 
 Start the Flask application:
--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-import redis
 import threading
+import librosa
+from io import BytesIO
+from pydub import AudioSegment
+from scipy.io.wavfile import write
+import numpy as np
+from scipy.io.wavfile import read
+from pydub import AudioSegment
 
 app = Flask(__name__)
-# Enable CORS for all domains on all routes
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+# Initialize buffer to accumulate audio data
+audio_buffer = BytesIO()
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# Initialize Redis client
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
-pubsub = redis_client.pubsub()
-pubsub.subscribe('audio_channel')
 
 @socketio.on('connect', namespace='/audio_stream')
 def handle_connect():
@@ -23,21 +26,43 @@ def handle_connect():
 @socketio.on('disconnect', namespace='/audio_stream')
 def handle_disconnect():
     print('Client disconnected')
+    process_and_save_audio()  # Process and save when client disconnects
 
 @socketio.on('audio_data', namespace='/audio_stream')
 def handle_audio_data(audio_data):
-    print(f"Received audio data from client")
-    redis_client.publish('audio_channel', audio_data)
+    try:
+        print(f"Received audio data from client, length: {len(audio_data)}")
+        audio_buffer.write(audio_data)
+        print(f"Data written to audio_buffer, length: {len(audio_buffer.getvalue())}")
+    except Exception as e:
+        print(f"Error receiving/storing audio data: {e}")
 
-def listen_for_audio_data():
-    for message in pubsub.listen():
-        if message['type'] == 'message':
-            print(f"Received audio data from Redis")
-            audio_data = message['data']
-
-# Create and start a new thread for the listener
-listener_thread = threading.Thread(target=listen_for_audio_data)
-listener_thread.start()
+def process_and_save_audio():
+    try:
+        print(f"Data in audio_buffer before read, length: {len(audio_buffer.getvalue())}")
+        audio_buffer.seek(0)
+        audio_data = audio_buffer.read()
+        print(f"Data read from audio_buffer, length: {len(audio_data)}")
+        
+        # Check if audio_data is empty
+        if not audio_data:
+            print("Audio data is empty")
+            return
+        
+        # Write the audio data to a temporary file
+        with open('temp.ogg', 'wb') as f:
+            f.write(audio_data)
+        
+        # Convert the audio data from Ogg to WAV format
+        audio = AudioSegment.from_ogg('temp.ogg')
+        audio.export('temp.wav', format='wav')
+        
+        # Read the audio data from the temporary file
+        sample_rate, audio = read('temp.wav')
+        
+        # Rest of the code...
+    except Exception as e:
+        print(f"Error processing/saving audio: {e}")
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
